f99df36d6f4304d8e2c495b00a77e73a1e376767
Bago <b.ourognao@gmail.com>
2018-02-25 15:53:06 +0900

    bago/new/top-page-design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:locale, :debug, :session])
+  end
 end
