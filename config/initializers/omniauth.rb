d311d3c5b2ab719d4c74469a59d1e17cd36f803e
bago <bago@bago-mac.local>
2019-01-20 18:06:26 +0900

    bago/add/facebook-omiauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,8 +8,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   	# callback_path: 'https://omoroi-fe-staging.herokuapp.com/auth/facebook/callback',
   	# callback_path: '/auth/facebook/callback',
   	# http://www.mysite.com/users/auth/facebook/callback
-  	provider_ignores_state: true,
-  	token_params: { parse: :json }
+  	provider_ignores_state: true
+  	#token_params: { parse: :json }
   	# client_options: {
    #    site: 'https://graph.facebook.com/v3.1',
    #    authorize_url: "https://www.facebook.com/v3.1/dialog/oauth"
