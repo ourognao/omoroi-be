02c35d7c1929d494db50ab559538fd64bb53813f
bago <bago@bago-mac.local>
2019-01-20 14:34:29 +0900

    bago/add/facebook-omiauth

--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 module Overrides
   class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
     def omniauth_success
+      binding.pry
       get_resource_from_auth_hash
     end
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,12 @@
 Devise.setup do |config|
   config.mailer_sender = Settings.mailer.gmail_no_reply
 end
+
+Rails.application.config.to_prepare do
+  Devise::OmniauthCallbacksController.class_eval do
+    def failure
+      set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+      redirect_to after_omniauth_failure_path_for(resource_name)
+    end
+  end
+end
