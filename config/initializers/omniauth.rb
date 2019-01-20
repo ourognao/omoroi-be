a2e2357873268cee9d8d9c9f36ee7cd18509eef5
bago <bago@bago-mac.local>
2019-01-20 15:45:26 +0900

    bago/add/facebook-omiauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,17 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email,name'
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {
-  	strategy_class: OmniAuth::Strategies::Facebook,
+  	# strategy_class: OmniAuth::Strategies::Facebook,
   	scope: 'email',
-  	#info_fields: 'email,name',
+  	info_fields: 'email,name',
   	callback_path: '/auth/facebook/callback',
+  	# callback_path: '/auth/facebook/callback',
+  	# http://www.mysite.com/users/auth/facebook/callback
   	provider_ignores_state: true,
-  	token_params: { parse: :json },
-  	client_options: {
-      site: 'https://graph.facebook.com/v3.1',
-      authorize_url: "https://www.facebook.com/v3.1/dialog/oauth"
-    }
+  	# token_params: { parse: :json },
+  	# client_options: {
+   #    site: 'https://graph.facebook.com/v3.1',
+   #    authorize_url: "https://www.facebook.com/v3.1/dialog/oauth"
+   #  }
   }
 end
