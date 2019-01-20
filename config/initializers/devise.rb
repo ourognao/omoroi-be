cbe8b25fd923c6974407b9fb24318c91e62754db
bago <bago@bago-mac.local>
2019-01-20 14:37:37 +0900

    bago/add/facebook-omiauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,8 +5,7 @@ end
 Rails.application.config.to_prepare do
   Devise::OmniauthCallbacksController.class_eval do
     def failure
-      set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-      redirect_to after_omniauth_failure_path_for(resource_name)
+      render json: { message: "Login failed." }, status: 401
     end
   end
 end
