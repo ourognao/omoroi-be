7de7de304bd79f39c074071395e2c1693601907c
bago <bago@bago-mac.local>
2019-01-22 20:47:44 +0900

    bago/add/facebook-omiauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery
   before_action :set_locale
+  before_action :skip_session
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def skip_session
+    request.session_options[:skip] = true
+  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -1,13 +1,40 @@
 module Overrides
   class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
+    skip_before_action :skip_session
+
+    def redirect_callbacks
+      super
+    end
+
     def omniauth_success
-      get_resource_from_auth_hash
+      super
+      update_auth_header
+    end
+
+    def omniauth_failure
+      super
     end
 
     protected
 
     def get_resource_from_auth_hash
-      Rails.logger.info "BAGO :: get_resource_from_auth_hash"
+      super
+	  @resource.credentials = auth_hash["credentials"]
+	  clean_resource
+	end
+
+	def render_data_or_redirect(message, data, user_data = {})
+      if Rails.env.production?
+        if ['inAppBrowser', 'newWindow'].include?(omniauth_window_type)
+          render_data(message, user_data.merge(data))
+        elsif auth_origin_url
+          redirect_to DeviseTokenAuth::Url.generate(auth_origin_url, data.merge(blank: true))
+        else
+          fallback_render data[:error] || 'An error occurred'
+        end
+      else
+        render json: @resource, status: :ok
+      end
     end
   end
 end
