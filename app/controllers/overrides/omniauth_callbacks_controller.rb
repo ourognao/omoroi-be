a538fa03068bffbd1d1e4e46ebc58b8e15708bc9
bago <bago@bago-mac.local>
2019-01-22 22:42:07 +0900

    bago/add/facebook-omiauth

--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ module Overrides
     protected
 
     def get_resource_from_auth_hash
-	  Rails.logger.info "BAGO :: #{request.env['omniauth.auth']['credentials']}"
+	  Rails.logger.info "BAGO :: #{request.env}"
 	end
   end
 end
